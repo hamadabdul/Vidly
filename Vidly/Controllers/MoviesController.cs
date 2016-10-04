24c4d85bbb0a2f9938e51e811cd8cea061ef2e06
hamadabdul <hamadabdul@gmail.com>
2016-10-04 07:31:22 +0100

    Post - Exercise 5 & Code Review Lec 58

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -43,10 +43,10 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel()
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
-                Genres = _context.Genres.ToList()
+               
+               Genres = _context.Genres.ToList()
             };
             return View("MovieForm", viewModel);
             
@@ -55,8 +55,18 @@ namespace Vidly.Controllers
 
       
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres=_context.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
