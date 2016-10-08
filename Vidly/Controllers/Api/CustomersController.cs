8f91871211f43208cf42e9c076f23a2b44e8a709
hamadabdul <hamadabdul@gmail.com>
2016-10-08 17:14:43 +0100

    Post IHttpActionResult Lecture 70

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -25,29 +25,30 @@ namespace Vidly.Controllers.Api
         }
 
         //get/api/customers/1
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            return Mapper.Map<Customer,CustomerDto>(customer);
+                return NotFound();
+                
+            return Ok(Mapper.Map<Customer,CustomerDto>(customer));
 
         }
 
         //post/api/customers/1
         [HttpPost]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
         {
-            if(!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            if (!ModelState.IsValid)
+                return BadRequest();
 
             var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
             customerDto.Id = customer.Id;
 
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id),customerDto);
         }
         
         //put /api/customers/1
